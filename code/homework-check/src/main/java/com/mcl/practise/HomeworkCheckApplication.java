bcde8272292314796132a75243b7f68bba56ebaa
ChunlinMing <1432608191@qq.com>
2017-12-25 23:22:34 +0800

    init Project

---
+++ code/homework-check/src/main/java/com/mcl/practise/HomeworkCheckApplication.java
@@ -0,0 +1,12 @@
+package com.mcl.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkCheckApplication.class, args);
+	}
+}
---
+++ code/homework-check/src/test/java/com/mcl/practise/HomeworkCheckApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mcl.practise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeworkCheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
